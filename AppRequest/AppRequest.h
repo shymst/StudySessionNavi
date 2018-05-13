6e99b17b0481d4fc86c6335a05ee41f453665db0
shymst <yamashita@lclco.com>
2018-05-13 18:55:42 +0900

    通信定義用のEmbeded frameworkとしてAppRequestを追加

---
+++ AppRequest/AppRequest.h
@@ -0,0 +1,18 @@
+//
+//  AppRequest.h
+//  AppRequest
+//
+//  Created by Shunya Yamashita on 2018/05/13.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppRequest.
+FOUNDATION_EXPORT double AppRequestVersionNumber;
+
+//! Project version string for AppRequest.
+FOUNDATION_EXPORT const unsigned char AppRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppRequest/PublicHeader.h>
+
+
