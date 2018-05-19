8a0301ddc56c4b61fe3bbe501ff000f78758d268
shymst <yamashita@lclco.com>
2018-05-19 12:25:17 +0900

    ViewModelを別ファイルへ分割

--- StudySessionNavi/SessionListViewController.swift
+++ StudySessionNavi/SessionListViewController.swift
@@ -9,55 +9,6 @@ import UIKit
 import UIComponent
 import RxSwift
 
-protocol SessionListViewModelInputs {
-    func decrementTargetMonth()
-    func incrementTargetMonth()
-    func getSessionList()
-}
-
-protocol SessionListViewModelOutputs {
-    var numberOfSections: Int { get }
-    var numberOfItemsInSection: Int { get }
-    var sessionList: [Session] { get }
-    var targetDate: Variable<Date> { get }
-}
-
-// 使用側はこの型を使用することでシンプルに扱える
-protocol SessionListViewModelType {
-    var inputs: SessionListViewModelInputs { get }
-    var outputs: SessionListViewModelOutputs { get }
-}
-
-final class SessionListViewModel: SessionListViewModelType, SessionListViewModelInputs, SessionListViewModelOutputs {
-
-    var inputs: SessionListViewModelInputs { return self }
-    var outputs: SessionListViewModelOutputs { return self }
-
-    // MARK: - HogeViewModelInputs
-
-    let numberOfSections: Int = 1
-    let numberOfItemsInSection: Int = 0
-    let sessionList: [Session] = []
-    var targetDate: Variable<Date> = Variable(Date())
-
-    // MARK: - HogeViewModelOutputs
-
-    func decrementTargetMonth() {
-        targetDate.value.month -= 1
-    }
-
-    func incrementTargetMonth() {
-        targetDate.value.month += 1
-    }
-
-    func getSessionList() {
-//        let dataStore = ConnpassDataStoreImpl()
-//        dataStore.search(keyword: "") { (result) in
-//            print(result)
-//        }
-    }
-}
-
 final class SessionListViewController: UIViewController {
 
     private lazy var calendarView: CalendarView = { [weak self] in
